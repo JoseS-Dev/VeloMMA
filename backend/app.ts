5d9a638caddcc74149caee8dfef78552cc2665cb
JoseS-Dev <joseasantana05@gmail.com>
2026-06-22 19:16:16 -0400

    feat: :sparkles: Actualizar la configuración y dependencias del backend para mejorar la funcionalidad

--- backend/app.ts
+++ backend/app.ts
@@ -1,35 +1,41 @@
-import express from 'express';
+import express, { json } from 'express';
 import type { Request, Response } from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
 import morgan from 'morgan';
 import cookieParser from 'cookie-parser';
 import rateLimit from 'express-rate-limit';
+import { registerRoutes } from './src/utils/functions/function.js';
+import { errorsMiddleware } from './src/middlewares/Exception/errors.middleware.js';
 import { settings } from './config/settings.js';
+import { Routes } from './src/api/routes.js';
 
 // Inició e servidor express
 const app: express.Application = express();
 
 // Middlewares
+app.use(json());
 app.use(cors());
 app.use(helmet());
 app.use(morgan('dev'));
 app.use(cookieParser());
+app.use(errorsMiddleware);
 
 // Rate Limit de peticiones
 app.use(rateLimit(settings.rateLimit));
 
 // Ruta de bienvenida
-app.get(`/`, (req: Request, res: Response) => {
+app.get(`${settings.basePath}`, (req: Request, res: Response) => {
     return res.send('Bienvenido a la API de VeloMMA');
 })
 
 // Rutas
+registerRoutes(app, Routes);
 
 // Escuchamos el servidor
 if(settings.nodeEnv === 'development'){
     app.listen(settings.port, () => {
-        console.log(`Servidor corriendo en http://localhost:${settings.port}`);
+        console.log(`Servidor corriendo en http://localhost:${settings.port}${settings.basePath}`);
     })
 }
 
