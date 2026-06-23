70f9e940355624a49dad12f7ee6fd017ee880c22
JoseS-Dev <joseasantana05@gmail.com>
2026-06-23 11:30:16 -0400

    feat: :sparkles: Implementar controlador, servicio, rutas y esquema de validación para la gestión de pesos

---
+++ backend/src/types/relational/weights/weight.types.ts
@@ -0,0 +1,9 @@
+// Defino la interfaz de los pesos oficiales de un luchador
+export interface WeightDTO {
+    fighter_id: number;
+    division_id: number;
+    is_current: boolean;
+}
+
+// Defino la interfaz para actualizar los pesos oficiales de un luchador
+export interface UpdateWeightDTO extends WeightDTO {}
