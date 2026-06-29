6b92a4849e5071ba3fd10ce44ded8c444e2225f3
JoseS-Dev <joseasantana05@gmail.com>
2026-06-29 14:50:15 -0400

    feat: :sparkles: Agregado las variables de redis al proyecto

--- backend/config/settings.ts
+++ backend/config/settings.ts
@@ -12,6 +12,9 @@ export const settings = {
     corsOrigin: process.env.CORS_ORIGIN || '*',
     apiKey: process.env.API_SECRET_KEY || '',
     cookieSecret: process.env.COOKIE_SECRET || '',
+    redisUrl: process.env.REDIS_URL || '',
+    redisPort: process.env.REDIS_PORT || 6379,
+    redisEnv: process.env.REDIS_ENV !== undefined ? process.env.REDIS_ENV === 'true' : false,
     rateLimit: {
         windowMs: 1 * 60 * 1000, // 5 minutos
         max: 100, // 100 peticiones
