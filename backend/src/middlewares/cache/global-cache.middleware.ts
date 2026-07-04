375676bfa955e963ba7ecdd610ed9732bc3b1fe6
JoseS-Dev <joseasantana05@gmail.com>
2026-07-04 10:25:14 -0400

    feat: :recycle: Mejora la gestión de la conexión a Redis y actualiza el middleware de caché para manejar correctamente la instancia de cliente

--- backend/src/middlewares/cache/global-cache.middleware.ts
+++ backend/src/middlewares/cache/global-cache.middleware.ts
@@ -12,16 +12,21 @@ export async function globalCacheMiddleware(
     const cacheKey = `cache:${req.originalUrl}`;
     try{
         // Verificamos buscar si esa query existe en la cache
-        const cachedResponse = await client.get(cacheKey);
+        const cachedResponse = await client?.get(cacheKey);
         if(cachedResponse){
             return res.json(JSON.parse(cachedResponse));
         }
         const originalJSON = res.json;
         res.json = function (body): Response {
+
+            if(!client){
+                return originalJSON.call(this, body);
+            }
+            
             // Guardamos la respuesta en al cache si solo fue exitosa
             if(body?.status === 200 && res.statusCode === 200){
                 const TTL_SECONDS = 120
-                client.setEx(cacheKey, TTL_SECONDS, JSON.stringify(body))
+                client?.setEx(cacheKey, TTL_SECONDS, JSON.stringify(body))
                 .catch(err => console.error('Error al guardar en cache', err));
             }
             return originalJSON(body);
