84e9c4a59f4b01ae5e1e901fcc7c546f591026d2
JoseS-Dev <joseasantana05@gmail.com>
2026-07-11 10:34:00 -0400

    feat: :sparkles: Añade middleware de limitación de tasa para gestionar los límites de solicitudes a la API y mejorar el rendimiento.

--- backend/src/api/routes.ts
+++ backend/src/api/routes.ts
@@ -2,12 +2,18 @@ import { Router } from 'express';
 import { settings } from '../../config/settings.js';
 import { routesConfig } from '../utils/utils.js';
 import { secureMiddleware } from '../middlewares/routes/route.middlewares.js';
+import { middlewareRateLimit } from '../middlewares/ratedLimit/rate.middleware.js';
 
 // Rutas de la API
 const apiRouter: Router = Router();
 
+const middlewareGlobalAPI = [
+    middlewareRateLimit,
+    secureMiddleware
+]
+
 routesConfig.forEach(({path, router}) => {
-    apiRouter.use(`${settings.basePath}${path}`, secureMiddleware ,router)
+    apiRouter.use(`${settings.basePath}${path}`, middlewareGlobalAPI ,router)
 })
 
 export { apiRouter };
---
+++ backend/src/middlewares/ratedLimit/rate.middleware.ts
@@ -0,0 +1,27 @@
+import type {Request, Response, NextFunction} from 'express';
+import { settings } from '../../../config/settings.js';
+import rateLimit from 'express-rate-limit';
+
+export const readLimiter = rateLimit({
+    windowMs: settings.readLimit.windowMs,
+    max: settings.readLimit.max,
+    message: 'Demasiadas perticiones de lectura, por favor espere cinco minutos y vuelva a intentarlo.',
+    standardHeaders: true,
+    legacyHeaders: false,
+});
+
+export const writeLimiter = rateLimit({
+    windowMs: settings.writeLimit.windowMs,
+    max: settings.writeLimit.max,
+    message: 'Demasiadas peticiones de escritura, por favor espere un minuto y vuelva a intentarlo.',
+    standardHeaders: true,
+    legacyHeaders: false,
+});
+
+
+// Middleware para limitar la cantidad de peticiones a la API
+export function middlewareRateLimit(req: Request, res: Response, next: NextFunction) {
+    const isWriteMethod = ['POST', 'PUT', 'PATCH', 'DELETE'].includes(req.method);
+    const limiter = isWriteMethod ? writeLimiter : readLimiter;
+    limiter(req, res, next);
+}
