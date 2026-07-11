102d8d46e47c208aafd2ac05ae7ccbc477cdfb1e
JoseS-Dev <joseasantana05@gmail.com>
2026-07-11 13:18:47 -0400

    fix: :bug: Arreglando los mocks para la zona horaria de los campos fechas

--- backend/mocks/__test__/relational/ranking/rank.mocks.ts
+++ backend/mocks/__test__/relational/ranking/rank.mocks.ts
@@ -3,5 +3,5 @@ export const mockCreateRanking = {
     fighter_id: 0,
     division_id: 0,
     rank: 1,
-    as_of_date: '2023-01-01'
+    as_of_date: '2023-01-01T00:00:00.000Z'
 }
--- backend/mocks/__test__/relational/stables/stable.mocks.ts
+++ backend/mocks/__test__/relational/stables/stable.mocks.ts
@@ -3,6 +3,6 @@ export const mockCreateStable = {
     fighter_id: 0, // Se asignará después
     team_id: 0, // Se asignará después
     is_current: true,
-    joined_date: '2023-01-01',
-    left_date: null
+    joined_date: '2023-01-01T00:00:00.000Z',
+    left_date: '2023-02-01T00:00:00.000Z'
 }
